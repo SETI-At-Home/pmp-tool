1f1a40ac20a0a7f1757b31685ce708580a77efa9
Vladimir Bozhinovski <bozhinovski.vladimir@gmail.com>
2019-01-04 11:16:04 +0100

    Incremental checking

--- apps/dashboard/src/app/customers/customers.module.ts
+++ apps/dashboard/src/app/customers/customers.module.ts
@@ -1,14 +1,11 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { NgModule } from '@angular/core';
 import { CustomersRoutingModule } from './customers-routing.module';
 import { CustomersComponent } from './customers.component';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    CustomersRoutingModule
-  ],
-  declarations: [CustomersComponent]
+  imports: [CommonModule, CustomersRoutingModule],
+  declarations: [CustomersComponent],
+  exports: [CustomersComponent]
 })
-export class CustomersModule { }
+export class CustomersModule {}
--- apps/dashboard/src/app/projects/projects.module.ts
+++ apps/dashboard/src/app/projects/projects.module.ts
@@ -1,14 +1,12 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { NgModule } from '@angular/core';
 import { ProjectsRoutingModule } from './projects-routing.module';
 import { ProjectsComponent } from './projects.component';
+import { MaterialModule } from '@workshop/material';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    ProjectsRoutingModule
-  ],
-  declarations: [ProjectsComponent]
+  imports: [CommonModule, ProjectsRoutingModule, MaterialModule],
+  declarations: [ProjectsComponent],
+  exports: [ProjectsComponent]
 })
-export class ProjectsModule { }
+export class ProjectsModule {}
