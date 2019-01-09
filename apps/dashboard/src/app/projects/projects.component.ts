a70714a426cec39236cacb9abc0cb448d4dd8c1f
Vladimir Bozhinovski <bozhinovski.vladimir@gmail.com>
2019-01-09 10:14:01 +0100

    component driver design refactored

--- apps/dashboard/src/app/projects/projects.component.ts
+++ apps/dashboard/src/app/projects/projects.component.ts
@@ -1,4 +1,3 @@
-import { Observable } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
 import { Project, ProjectsService } from '@workshop/core-data';
 
@@ -8,6 +7,7 @@ import { Project, ProjectsService } from '@workshop/core-data';
   styleUrls: ['./projects.component.scss']
 })
 export class ProjectsComponent implements OnInit {
+  primaryColor = 'red';
   projects$;
   selectedProject: Project;
 
@@ -15,22 +15,57 @@ export class ProjectsComponent implements OnInit {
 
   ngOnInit() {
     this.getProjects();
+    this.resetProject();
   }
 
   selectProject(project) {
     this.selectedProject = project;
   }
 
+  resetProject() {
+    const emptyProject: Project = {
+      id: null,
+      title: '',
+      details: '',
+      percentComplete: 0,
+      approved: false
+    };
+    this.selectProject(emptyProject);
+  }
+
   getProjects() {
     this.projects$ = this.projectService.all();
   }
 
+  createProject(project) {
+    this.projectService.create(project).subscribe(result => {
+      this.getProjects();
+      this.resetProject();
+    });
+  }
+
+  updateProject(project) {
+    this.projectService.update(project).subscribe(result => {
+      this.getProjects();
+      this.resetProject();
+    });
+  }
+
+  saveProject(project) {
+    if (!project.id) {
+      this.createProject(project);
+    } else {
+      this.updateProject(project);
+    }
+  }
+
   deleteProject(project) {
-    this.projectService.delete(project.id)
+    this.projectService
+      .delete(project.id)
       .subscribe(result => this.getProjects());
   }
 
   cancel() {
-    this.selectProject(null);
+    this.resetProject();
   }
 }
--- apps/dashboard/src/app/projects/projects.module.ts
+++ apps/dashboard/src/app/projects/projects.module.ts
@@ -3,10 +3,13 @@ import { NgModule } from '@angular/core';
 import { ProjectsRoutingModule } from './projects-routing.module';
 import { ProjectsComponent } from './projects.component';
 import { MaterialModule } from '@workshop/material';
+import { FormsModule } from '@angular/forms';
+import { ProjectsListComponent } from './projects-list/projects-list.component';
+import { ProjectDetailsComponent } from './project-details/project-details.component';
 
 @NgModule({
-  imports: [CommonModule, ProjectsRoutingModule, MaterialModule],
-  declarations: [ProjectsComponent],
+  imports: [CommonModule, ProjectsRoutingModule, MaterialModule, FormsModule],
+  declarations: [ProjectsComponent, ProjectsListComponent, ProjectDetailsComponent],
   exports: [ProjectsComponent]
 })
 export class ProjectsModule {}
